1e256ed20be4e187c6c469b11c92024104d14e6d
thiennguyen27 <nguyenleminhthien27092005@gmail.com>
2025-11-08 17:47:22 -0600

    update home routes

--- backend/routers/homeRoutes.js
+++ backend/routers/homeRoutes.js
@@ -1,14 +1,8 @@
 import express from "express";
 import { goToHome} from "../controllers/homeController.js";
-import {managementLogin} from "../controllers/managementController.js";
-import {startOrder} from "../controllers/orderController.js";
-
 const router = express.Router();
 
 // GET/: go to home page
 router.get("/weather", goToHome);           
-
-// POST /api/management/login: Log in as manager
-router.post("/login/", managementLogin);
 
 export default router;
